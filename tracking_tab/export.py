1fe15583553a8f00fbadc93e8d268ebeb8ff7e50
MarcosTavar3s <maurelio1411@gmail.com>
2025-08-14 08:11:09 -0300

    box coordinates fixed

--- tracking_tab/export.py
+++ tracking_tab/export.py
@@ -28,12 +28,13 @@ def export_tracking_data(rois, roi_count, image_height, image_width):
             if roi.left < 0:
                 roi.left = 0
 
+            # image_height - height_in frame happens bc of orientation in xy-axis
             json_list.append({
                 "type": "box",
-                "top": int(roi.top),
-                "bottom": int(roi.bottom),
+                "top":  image_height-int(roi.top),
+                "bottom": image_height-int(roi.bottom),
                 "right": int(roi.right),
-                "left": int(roi.left)
+                "left":  int(roi.left)
             })
                         
         if str(type(roi)) == "<class 'bokeh.models.annotations.geometry.PolyAnnotation'>":           
@@ -51,12 +52,6 @@ def export_tracking_data(rois, roi_count, image_height, image_width):
                 "radius": int(roi.glyph.radius)
             })
             
-            # pprint(vars(roi))
-            pprint(vars(roi.glyph))
-            print(roi.glyph.x, roi.glyph.y, roi.glyph)
-        
-    print(json_list)
-    
     json_dict["rois"] = json_list
     with open("rois.json", "w") as file:
         json.dump(json_dict, file, indent=2)
