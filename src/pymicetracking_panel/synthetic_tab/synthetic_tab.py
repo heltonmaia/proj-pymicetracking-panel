42bafeea8681dd75aef8c1f155f15428074bf424
Richardson Menezes <rmenezes@tractian.com>
2025-08-31 21:32:14 -0300

    Refactor get_tab function in synthetic_tab.py: enhance code readability and add type hints

--- src/pymicetracking_panel/synthetic_tab/synthetic_tab.py
+++ src/pymicetracking_panel/synthetic_tab/synthetic_tab.py
@@ -1,4 +1,10 @@
 import panel as pn
 
-def get_tab():
-    return pn.Column(pn.pane.Markdown('## Synthetic Data\nSynthetic data generation and tools will appear here.'), margin=(10, 0))
+
+def get_tab() -> pn.Column:
+    return pn.Column(
+        pn.pane.Markdown(
+            "## Synthetic Data\nSynthetic data generation and tools will appear here."
+        ),
+        margin=(10, 0),
+    )
